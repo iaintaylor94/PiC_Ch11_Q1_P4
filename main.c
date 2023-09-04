487482151168bd9dd6ade7d5d8304ba208f1b656
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-04 14:03:22 +0000

    copied program

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
+// Program to illustrate structure pointers
+
 #include <stdio.h>
 
+// Define Structures
+struct date {
+int month;
+int day;
+int year;
+};
+
 int main(void) {
-  printf("Hello World\n");
+
+  struct date today, *datePtr;
+
+  datePtr = &today;
+
+  datePtr->month = 9;
+  datePtr->day = 25;
+  datePtr->year = 2004;
+
+  printf ("Todays date is %d/%d/%.2d.\n", datePtr->month, datePtr->day, datePtr->year % 100);
+  
   return 0;
 }
